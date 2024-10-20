bd4635701f914d603636e838ede2728042d0b49c
Hemanth8989 <hemanthkumarkakarla121@gmail.com>
2024-10-20 12:34:23 +0530

    Add project files.

---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Domain
+{
+    internal class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
